30480537e7e0853e0778c9ec6f74277415ed32d4
MsN <mohsen.s7s.ab@gmail.com>
2024-07-28 23:03:51 +0330

    fix  versioning

--- Basket.API/Program.cs
+++ Basket.API/Program.cs
@@ -1,7 +1,7 @@
-
 using System.Reflection;
 using Asp.Versioning;
 using AspNet.CorrelationIdGenerator;
+using Basket.API.Swagger;
 using Basket.Application.Handlers;
 using Basket.Application.Mappers;
 using Basket.Core.Repositories;
@@ -9,21 +9,27 @@ using Basket.Infrastructure.Repositories;
 using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
-using Microsoft.OpenApi.Models;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c => c.SwaggerDoc("v1",
-    new OpenApiInfo{Title = "Basket.API", Version = "v1"}));
-
 builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddApiVersioning(options =>
-{
-    options.AssumeDefaultVersionWhenUnspecified = true;
-    options.DefaultApiVersion = new ApiVersion(1, 0);
-    options.ReportApiVersions = true;
-});
+    {
+        options.ReportApiVersions = true;
+        options.ApiVersionReader = new UrlSegmentApiVersionReader();
+    })
+    .AddMvc()
+    .AddApiExplorer(x =>
+    {
+        x.GroupNameFormat = "'v'VVV";
+        x.SubstituteApiVersionInUrl = true;
+    });
+    
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("CorsPolicy", policy =>
@@ -32,6 +38,7 @@ builder.Services.AddCors(options =>
         policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
     });
 });
+
 builder.Services.AddStackExchangeRedisCache(options =>
 {
     options.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
@@ -41,12 +48,21 @@ builder.Services.AddAutoMapper(typeof(BasketMappingProfile));
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(CreateShoppingCartCommandHandler).GetTypeInfo().Assembly));
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 builder.Services.AddScoped<ICorrelationIdGenerator, CorrelationIdGenerator>();
+builder.Services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
 
 builder.Services.AddHealthChecks()
     .AddRedis(builder.Configuration.GetValue<string>("CacheSettings:ConnectionString"), "Redis Health", HealthStatus.Degraded);
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.OperationFilter<SwaggerDefaultValues>();
+});
+
+// Build
 var app = builder.Build();
 
 app.MapControllers();
+
 app.MapHealthChecks("/health", new HealthCheckOptions()
 {
     Predicate = _ => true,
@@ -68,5 +84,7 @@ app.UseStaticFiles();
 app.UseAuthorization();
 
 app.UseHttpsRedirection();
+
+app.UseExceptionHandler("/Home/Error");
 
 app.Run();
