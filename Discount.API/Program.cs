4d8a6ffd84f9b70b084cc3fa4bb725e36e9e55e2
MsN <mohsen.s7s.ab@gmail.com>
2024-08-01 14:56:32 +0330

    add discount service

---
+++ Discount.API/Program.cs
@@ -0,0 +1,47 @@
+using System.Reflection;
+using AspNet.CorrelationIdGenerator;
+using Discount.API.Services;
+using Discount.Application.Handlers;
+using Discount.Application.Mapper;
+using Discount.Core.Repositories;
+using Discount.Infrastructure.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(CreateDiscountCommandHandler).GetTypeInfo().Assembly));
+builder.Services.AddScoped<ICorrelationIdGenerator, CorrelationIdGenerator>();
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+builder.Services.AddAutoMapper(typeof(DiscountProfile));
+builder.Services.AddGrpc(); 
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapGrpcService<DiscountService>();
+    endpoints.MapGet("/", async context =>
+    {
+        await context.Response.WriteAsync(
+            "Communication with gRPC endpoints must be made through a gRPC client.");
+    });
+});
+
+app.Run();
+
